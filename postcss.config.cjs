203c5cb891af5553e691327a76a2af9374812c1a
Hakorimanasharif12 <hakorimanasharif12@gmail.com>
2025-07-01 21:14:55 +0200

    Fix tailwind config for colors and update header text color

---
+++ postcss.config.cjs
@@ -0,0 +1,9 @@
+const tailwindcssPostcss = require('@tailwindcss/postcss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  plugins: [
+    tailwindcssPostcss(),
+    autoprefixer(),
+  ],
+};
---
+++ src/componets/Header/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const  Header = () => {
+  return (
+   <header>
+    <div className='top-strip py-2 bg-primary'>
+    <div className='container'>
+        <div className='flex items-center justify-between'> 
+            <div className='col1 w-[50%]'>
+                <p className='text-green-500 text-[14px]'>Get up to 50% off new season styles, limited time only</p>
+            </div>
+        </div>
+    </div>
+        </div>
+   </header>
+  )
+}
+
+export default  Header
